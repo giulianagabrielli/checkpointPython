b02e7d580f146ea07f1689b1099c7cb859bab101
giulianagabrielli <glngabrielli@gmail.com>
2020-04-23 22:33:04 -0300

    checkpoint python

--- checkpoint.py
+++ checkpoint.py
@@ -1,18 +1,19 @@
 listaEmails = []
 inserir = "Sim"
-naLista = "Sim"
 
-while inserir == "Sim":
+while len(listaEmails) <= 30:
+    listaEmails.append(input("Digite o email: ").lower())
+
+while inserir == "Sim" and len(listaEmails) > 30:
     listaEmails.append(input("Digite o email: ").lower())
     inserir = input("Deseja cadastrar mais um email?")
 
-pergunta = input("Qual é o email que está buscando? ")
+emailBuscado = input("Qual é o email que está buscando? ")
 
-for email in listaEmails:
-    if pergunta == email:
-        print(email)
-    else:
-        print("O email não existe.")
+if emailBuscado in listaEmails:
+    print("O e-mail " + emailBuscado + " consta na lista.")
+else:
+    print("O email não existe.")
 
 
 
